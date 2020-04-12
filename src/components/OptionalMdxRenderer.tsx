808efdb706f4ce5e5facd9103e77d04ccf0b9ab5
Everspace <7560635+Everspace@users.noreply.github.com>
2020-04-11 18:41:19 -0700

    Create OptionalMdxRenderer to render mdx nodes... optionally.

---
+++ src/components/OptionalMdxRenderer.tsx
@@ -0,0 +1,5 @@
+import { MDXRenderer } from "gatsby-plugin-mdx"
+import React from "react"
+
+export const OptionalMdxRenderer = (mdxNode: any) =>
+  mdxNode?.body ? <MDXRenderer>{mdxNode.body}</MDXRenderer> : null
