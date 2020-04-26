f1ddfdfd2c2cf38ce13d22c6c2b2596dd5c0d57f
Everspace <7560635+Everspace@users.noreply.github.com>
2020-04-25 20:04:20 -0700

    correct OptionalMdxRenderer to work like intended

--- src/components/OptionalMdxRenderer.tsx
+++ src/components/OptionalMdxRenderer.tsx
@@ -1,5 +1,5 @@
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import React from "react"
 
-export const OptionalMdxRenderer = (mdxNode: any) =>
+export const OptionalMdxRenderer = ({ mdxNode }: any) =>
   mdxNode?.body ? <MDXRenderer>{mdxNode.body}</MDXRenderer> : null
